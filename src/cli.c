6ca975f209e24f66f5676ce23fe6dadfd0287bd5
CR0YD <s.fiebranz0611@gmail.com>
2025-09-29 14:26:17 +0200

    :sparkles: feat: added error messages

--- src/cli.c
+++ src/cli.c
@@ -129,6 +129,7 @@ encode_args parse_encode_arguments(int argc, char* argv[]) {
     if (argc > offset) {
         FILE* src = fopen(argv[offset], "rb");
         if (!src) {
+            fprintf(stderr, "##ERROR## Cannot read <src> '%s'.\n", argv[offset]);
             free_encode_args(&args);
             return NULL;
         }
@@ -136,6 +137,7 @@ encode_args parse_encode_arguments(int argc, char* argv[]) {
         args->src = src;
         offset++;
     } else {
+        fprintf(stderr, "##ERROR## Argument <src> missing.\n");
         free_encode_args(&args);
         return NULL;
     }
@@ -143,6 +145,7 @@ encode_args parse_encode_arguments(int argc, char* argv[]) {
     if (argc > offset) {
         FILE* img = fopen(argv[offset], "r");
         if (!img) {
+            fprintf(stderr, "##ERROR## Cannot read <img> '%s'.\n", argv[offset]);
             free_encode_args(&args);
             return NULL;
         }
@@ -152,6 +155,7 @@ encode_args parse_encode_arguments(int argc, char* argv[]) {
         strcpy(args->img, argv[offset]);
         offset++;
     } else {
+        fprintf(stderr, "##ERROR## Argument <img> missing.\n");
         free_encode_args(&args);
         return NULL;
     }
@@ -159,6 +163,7 @@ encode_args parse_encode_arguments(int argc, char* argv[]) {
     if (argc > offset) {
         FILE* out = fopen(argv[offset], "r");
         if (out || errno == EISDIR) {
+            fprintf(stderr, "##ERROR## <out> '%s' already exists.\n", argv[offset]);
             fclose(out);
             free_encode_args(&args);
             return NULL;
@@ -167,11 +172,13 @@ encode_args parse_encode_arguments(int argc, char* argv[]) {
         args->out = fopen(argv[offset], "wb");
         offset++;
     } else {
+        fprintf(stderr, "##ERROR## Argument <out> missing.\n");
         free_encode_args(&args);
         return NULL;
     }
 
    if (argc > offset) {
+        fprintf(stderr, "##ERROR## Too many arguments.\n");
        free_encode_args(&args);
        return NULL;
    }
@@ -181,11 +188,13 @@ encode_args parse_encode_arguments(int argc, char* argv[]) {
 
 decode_args parse_decode_arguments(int argc, char* argv[]) {
     if (argc < 3) {
+        fprintf(stderr, "##ERROR## Not enough arguments for mode 'decode'.\n");
         return NULL;
     }
 
     decode_args args = calloc(1, sizeof(struct decode_args_struct));
     if (args == NULL) {
+        fprintf(stderr, "##ERROR## Failed to allocate memory.\n");
         return args;
     }
 
@@ -193,6 +202,7 @@ decode_args parse_decode_arguments(int argc, char* argv[]) {
 
     if (strcmp(argv[offset], "-b") == 0 && argc > offset + 1) {
         if (validate_bitmask(argv[++offset]) != 0) {
+            fprintf(stderr, "##ERROR## Invalid bitmask format.\n");
             free_decode_args(&args);
             return NULL;
         }
@@ -228,6 +238,7 @@ decode_args parse_decode_arguments(int argc, char* argv[]) {
     if (argc > offset) {
         FILE* src = fopen(argv[offset], "r");
         if (!src) {
+            fprintf(stderr, "##ERROR## Cannot read <src> '%s'.\n", argv[offset]);
             free_decode_args(&args);
             return NULL;
         }
@@ -237,6 +248,7 @@ decode_args parse_decode_arguments(int argc, char* argv[]) {
         strcpy(args->src, argv[offset]);
         offset++;
     } else {
+        fprintf(stderr, "##ERROR## Argument <src> missing.\n");
         free_decode_args(&args);
         return NULL;
     }
@@ -244,6 +256,7 @@ decode_args parse_decode_arguments(int argc, char* argv[]) {
     if (argc > offset) {
         FILE* out = fopen(argv[offset], "r");
         if (out || errno == EISDIR) {
+            fprintf(stderr, "##ERROR## <out> '%s' already exists.\n", argv[offset]);
             fclose(out);
             free_decode_args(&args);
             return NULL;
@@ -251,11 +264,13 @@ decode_args parse_decode_arguments(int argc, char* argv[]) {
 
         args->out = fopen(argv[offset++], "wb");
     } else {
+        fprintf(stderr, "##ERROR## Argument <out> missing.\n");
         free_decode_args(&args);
         return NULL;
     }
 
     if (argc > offset) {
+        fprintf(stderr, "##ERROR## Too many arguments.\n");
         free_decode_args(&args);
         return NULL;
     }
@@ -281,11 +296,13 @@ arg_types parse_arguments(int argc, char* argv[], unsigned mode) {
 
 cli_args get_arguments(int argc, char* argv[]) {
     if (argc < 2) {
+        fprintf(stderr, "##ERROR## Argument <mode> missing.\n");
         return NULL;
     }
 
     cli_args args = calloc(1, sizeof(struct cli_args_struct));
     if (args == NULL) {
+        fprintf(stderr, "##ERROR## Failed to allocate memory.\n");
         return args;
     }
 
@@ -294,6 +311,7 @@ cli_args get_arguments(int argc, char* argv[]) {
     const int mode = parse_mode(argv[1]);
 
     if (mode == -1) {
+        fprintf(stderr, "##ERROR## Unknown <mode> %s\n", argv[1]);
         free_cli_args(&args);
         return NULL;
     }
@@ -303,6 +321,7 @@ cli_args get_arguments(int argc, char* argv[]) {
     args->args = parse_arguments(argc, argv, mode);
 
     if (args->args.decode == NULL && args->args.encode == NULL) {
+        fprintf(stderr, "##ERROR## Failed to parse arguments.\n");
         free_cli_args(&args);
         return NULL;
     }
